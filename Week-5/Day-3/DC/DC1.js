5b40f3ae4e30753a40bd8354ce2d2d57bce9eab9
obarinshteyn@gmail.com <obarinshteyn@gmail.com>
2025-03-04 18:10:32 +0200

    hw

---
+++ Week-5/Day-3/DC/DC1.js
@@ -0,0 +1,57 @@
+const gameInfo = [
+    {
+        username: "john",
+        team: "red",
+        score: 5,
+        items: ["ball", "book", "pen"]
+    },
+    {
+        username: "becky",
+        team: "blue",
+        score: 10,
+        items: ["tape", "backpack", "pen"]
+    },
+    {
+        username: "susy",
+        team: "red",
+        score: 55,
+        items: ["ball", "eraser", "pen"]
+    },
+    {
+        username: "tyson",
+        team: "green",
+        score: 1,
+        items: ["book", "pen"]
+    },
+];
+
+// Create an array using forEach that contains all the usernames from 
+// the gameInfo array, add an exclamation point (ie. “!”) to the end of 
+// every username.
+// The new array should look like this :
+// const usernames = ["john!", "becky!", "susy!", "tyson!"]
+const usernames = [];
+
+gameInfo.forEach(player => {
+    usernames.push(player.username + "!")
+})
+
+console.log(usernames)
+// 2. Create an array using forEach that contains the usernames 
+// of all players with a score bigger than 5.
+// The new array should look like this :
+let winners = []
+gameInfo.forEach(player => {
+    if (player.score > 5) {
+        winners.push(player.username)
+    }
+})
+console.log(winners)
+
+
+
+// 3. Find and display the total score of the users.
+//  (Hint: The total score is 71)
+var totalScore = gameInfo.reduce((total, num) =>
+    total + num.score, 0)
+console.log(totalScore)
